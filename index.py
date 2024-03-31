158f40958c2538d3235f0757416214d032719517
Parveen Kumar <prince.malethia.36@gmail.com>
2024-03-31 19:36:38 +0530

    Annother test commit

--- index.py
+++ index.py
@@ -3,10 +3,8 @@ import random
 def main():
     print("This function doesnt do anything fancy")
     print(random.randint(1,10))
-
-    names = ("Parveen", "Parveen",
-      )
-    print("Another test")
+    names = ("Parveen", "Parveen",)
+    print(names)
     print("Another test 2")
 
 
