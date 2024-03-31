57441e58a6f8aedd6d3d4d29741767d9a24b01b9
Parveen Kumar <prince.malethia.36@gmail.com>
2024-03-31 19:38:00 +0530

    Linting fixed

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
 import random
 
+
 def main():
     print("This function doesnt do anything fancy")
-    print(random.randint(1,10))
-    names = ("Parveen", "Parveen",)
+    print(random.randint(1 ,10))
+    names = ("Parveen", "Parveen", )
     print(names)
     print("Another test 2")
 
